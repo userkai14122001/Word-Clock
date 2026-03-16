05bb28365d3df30b05613e72846d64cb7f3c7c18
Kai R. <85836425+userkai14122001@users.noreply.github.com>
2026-03-16 22:41:36 +0100

    Rename header from WIFI_MANAGER to MQTT_MANAGER

--- mqtt_manager.h
+++ mqtt_manager.h
@@ -1,41 +1,13 @@
-#ifndef WIFI_MANAGER_H
-#define WIFI_MANAGER_H
+#ifndef MQTT_MANAGER_H
+#define MQTT_MANAGER_H
 
 #include <Arduino.h>
-#include <WiFi.h>
-#include <WebServer.h>
-#include <DNSServer.h>
-#include <Preferences.h>
-#include <ArduinoJson.h>
-#include <PubSubClient.h>
-// Funktionen aus wifi_manager.cpp
-void setupWebRoutes();
-void startSetupMode();
-void tryConnectWiFi();
-void handleSetupWeb();
-void performWifiScan();
+#include "config.h"
 
-// Globale Objekte aus WordClock.ino
-extern WebServer server;
-extern DNSServer dnsServer;
-extern Preferences prefs;
-
-extern bool setupMode;
-extern unsigned long setupStartTime;
-
-extern String wifi_ssid;
-extern String wifi_pass;
-
-extern String mqtt_server;
-extern String mqtt_user;
-extern String mqtt_pass;
-
-// MQTT-Variablen (kommen aus WordClock.ino)
-extern WiFiClient espClient;
-extern PubSubClient mqtt;
-
-extern String device_id;
-extern String command_topic;
-extern String state_topic;
+void initMQTT();
+void mqttReconnect();
+void publishState();
+void sendDiscovery();
+void mqttLoop();
 
 #endif
